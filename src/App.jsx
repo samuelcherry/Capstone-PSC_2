536ee8e40e030440530921e00c16b3f8b58e52d4
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2025-01-03 11:27:50 -0700

    added login field

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import { Routes } from 'react-router-dom'
-import Maptest from './Maptest'
+import { useState } from "react";
+import { Routes } from "react-router-dom";
+import Maptest from "./Maptest";
+import UserProfile from "./components/UserProfile";
 
 export default function App() {
-
   return (
     <div>
-    <Maptest/>
+      <Maptest />
       <h1>Priyanka, Sam, Curt's Capstone</h1>
+      <UserProfile />
     </div>
-  )
+  );
 }
