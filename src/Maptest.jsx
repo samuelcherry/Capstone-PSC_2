53dd715863298dc93d3199d4f6782c678f8863af
Curt Miller <millercw3@gmail.com>
2025-01-03 11:51:46 -0500

    testing map functionality

--- src/Maptest.jsx
+++ src/Maptest.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { APIProvider, Map } from '@vis.gl/react-google-maps';
-import { GOOGLE_MAPS_API_KEY } from '/.env'
+// import { KEY } from '../.env'
+
+// require("dotenv").config(); 
+
+// const pg = require("pg");
+// const KEY = new pg.Client(process.env.GOOGLE_MAPS_API_KEY);
+
+// console.log(KEY);
+
 
 export default function Maptest() {
 
     return (
-        <APIProvider apiKey={GOOGLE_MAPS_API_KEY}>
+        <APIProvider apiKey='AIzaSyC1w2rEklDouOkbNQxSg9EVFum0Bn5ouOQ'>
             <Map
-                style={{ width: '100vw', height: '100vh' }}
+                style={{ width: '80vw', height: '80vh' }}
                 defaultCenter={{ lat: 22.54992, lng: 0 }}
                 defaultZoom={3}
                 gestureHandling={'greedy'}
