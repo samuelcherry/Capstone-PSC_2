4563ca54edd90e065ec8145fbf49e6ae535b6de4
Curt Miller <millercw3@gmail.com>
2025-01-03 10:34:31 -0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { Routes } from 'react-router-dom'
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>Priyanka, Sam, Curt's Capstone</h1>
+    </div>
+  )
+}
